3c02d35f3a32390cb5b4c400f5848f5ac60e050b
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2024-05-26 10:17:51 +0300

    51

--- complex_puppet_bolt_task/plans/configure_routers.pp
+++ complex_puppet_bolt_task/plans/configure_routers.pp
@@ -2,9 +2,6 @@ plan complex_puppet_bolt_task::configure_routers (
   TargetSpec $targets
 ) {
   $results = run_task('complex_puppet_bolt_task::get_interface_facts', $targets)
-
-  # Log results and errors
   out::message("Results: ${results}")
-
   return $results
 }
