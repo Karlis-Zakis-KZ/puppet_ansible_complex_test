d240b8bd64b4ad7bae067f4a2c176f549e061d92
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2024-05-26 10:56:27 +0300

    1313

--- complex_puppet_bolt_task/plans/get_interface_facts.pp
+++ complex_puppet_bolt_task/plans/get_interface_facts.pp
@@ -32,15 +32,15 @@ plan complex_puppet_bolt_task::get_interface_facts(
       out::message("Interface facts for ${target.uri}: ${interface_facts}")
 
       # Update the hash using stdlib::merge function
-      $all_interface_facts = stdlib::merge($all_interface_facts, { $target.uri => $interface_facts })
-      out::message("Updated all_interface_facts: ${stdlib::to_json($all_interface_facts)}")
+      $all_interface_facts = $all_interface_facts + { $target.uri => $interface_facts }
+      out::message("Updated all_interface_facts: ${all_interface_facts}")
     } else {
       out::message("Failed to fetch interface facts for ${target}.")
     }
   }
 
   # Output the collected interface facts as JSON
-  $json_facts = stdlib::to_json($all_interface_facts)
+  $json_facts = to_json($all_interface_facts)
   out::message("All interface facts: ${json_facts}")
 
   # Return the collected interface facts
