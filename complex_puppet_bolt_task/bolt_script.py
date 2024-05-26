f0cee1ff91189a94896912faa27f6e4dae7fa0bb
Karlis-Zakis-KZ <karliszakis13@gmail.com>
2024-05-26 18:57:23 +0300

    4123

--- complex_puppet_bolt_task/bolt_script.py
+++ complex_puppet_bolt_task/bolt_script.py
@@ -128,7 +128,7 @@ if __name__ == "__main__":
     task_name = "puppet_bolt"
 
     # Collect interface facts for all targets
-    targets = ["R2", "R3", "R4", "R6", "R7", "R8", "R9", "R10", "R11", "R12", "R13", "R14", "R15", "R16"]
+    targets = ["R1", "R3", "R4"]#, "R6", "R7", "R8", "R9", "R10", "R11", "R12", "R13", "R14", "R15", "R16"]
     facts = collect_interface_facts(targets)
     
     logging.debug(f"Collected interface facts: {facts}")
